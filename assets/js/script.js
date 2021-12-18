c250337df33b656456d91d5ad1f7a3122d1ca299
Nate Boucher <natejboucher@gmail.com>
2021-12-18 10:42:44 -0500

    moved objects into array, added increment variable to answerBtnHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,57 +3,74 @@ var questionEl = document.getElementById('questions');
 var answersEl = document.getElementById('answers');
 var startQuizBtn = document.getElementById('start-button');
 var answerAlert = document.getElementById('response-text');
+var timeLeft = 60;
+let i = 0;
 
-// question objects
-var questionOne = {
+// question objects array
+const questions = [
+{
     question: 'What does the term HTML stand for?',
     answerOne: 'Hyper Text Pre-processor.',
-    correctAnswer: 'Hyper Text Markup Language.',
+    answerTwo: 'Hyper Text Markup Language.',
     answerThree: 'Hyper Text Multiverse.',
-    answerFour: 'Hyper Text Multiple Language.'
-};
-var questionTwo = {
+    answerFour: 'Hyper Text Multiple Language.',
+    correct: 'Hyper Text Markup Language.'
+},
+{
     question: 'A _____ is a binary varial that has two possible values called True or False',
     answerOne: 'Var.',
     answerTwo: 'Object.',
     answerThree: 'Array.',
-    correctAnswer: 'Boolean.'
-};
-var questionThree = {
+    answerFour: 'Boolean.',
+    correct: 'Boolean.'
+},
+{
     question: 'What is the method of writing our Javascript code in plain english?',
     answerOne: 'A novel.',
     answerTwo: 'Markup Text.',
-    correctAnswer: 'Pseudocode.',
-    answerFour: 'Comments.'
-}
-var questionFour = {
+    answerThree: 'Pseudocode.',
+    answerFour: 'Comments.',
+    correct: 'Pseudocode.'
+},
+{
     question: 'What is used to target HTML elements by ID in Javascript?',
     answerOne: 'appendChild.',
     answerTwo: 'getElementById',
     answerThree: 'querySelector',
-    correctAnswer: 'Media Queries'
-}
-var questionFive = {
+    answerFour: 'Media Queries',
+    correct: 'getElementById'
+},
+{
     question: 'Which of the following is NOT a Javascript eventListener property?',
     answerOne: 'click',
     answerTwo: 'mouseover',
     answerThree: 'mouseout',
-    answerFour: ':hover'
+    answerFour: ':hover',
+    correct: ':hover'
 }
+];
 
 //answer button handler
 function answerBtnHandler(event)    {
-    if (event.target.matches(".correct-btn")) {
-        answerAlert.textContent = "Correct!"
+    if (event.target.textContent === questions[i].correct) {
+        answerAlert.textContent = "Correct!";
+        i++;
+        if (i === 5 || timeLeft === 0)  {
+            endGame();
+        } else {
+            loadQuestion();
+        }
     } else  {
         answerAlert.textContent = "Wrong!";
+        reduceTimer();
     }
+}
+function endGame()  {
+    console.log("This works");
 }
 
 // function to control countdown
 function countDown()  {
-    //set time left variable
-    var timeLeft = 60;
     // set interval method 
     var timeInterval = setInterval(function()   {
         if (timeLeft > 1)   {
@@ -69,45 +86,39 @@ function countDown()  {
 
 
 //function to reduce timer and display wrong answer
+function reduceTimer()  {
+    if (timeLeft > 10)  {
+        timeLeft = (timeLeft - 10);
+    }
+}
 
-//function for first question
-function firstQuestion()    {
+//function for questions
+function loadQuestion()    {
+    answersEl.innerHTML = "";
     //display question
-    questionEl.textContent = questionOne.question;
+    questionEl.textContent = questions[i].question;
 
     //generate answer buttons
     var answerOneEl = document.createElement("button");
-    answerOneEl.textContent = questionOne.answerOne;
+    answerOneEl.textContent = questions[i].answerOne;
     answerOneEl.className = "btn";
     answersEl.appendChild(answerOneEl);
 
     var correctAnswer = document.createElement("button");
-    correctAnswer.textContent = questionOne.correctAnswer;
+    correctAnswer.textContent = questions[i].answerTwo;
     correctAnswer.className = "btn correct-btn";
     answersEl.appendChild(correctAnswer);
 
     var answerThreeEl = document.createElement("button");
-    answerThreeEl.textContent = questionOne.answerThree;
+    answerThreeEl.textContent = questions[i].answerThree;
     answerThreeEl.className = "btn";
     answersEl.appendChild(answerThreeEl);
 
     var answerFourEl = document.createElement("button");
-    answerFourEl.textContent = questionOne.answerFour;
+    answerFourEl.textContent = questions[i].answerFour;
     answerFourEl.className = "btn";
     answersEl.appendChild(answerFourEl);
-
-    //function for selecting result from answerBtnHandler
-    if ()  {
-        console.log("This will load the next question");
-    }
 }
-//function for second question
-
-//function for third question
-
-//function for fourth question
-
-//function for fifth question
 
 //function to remove "start quiz" button
 function removeStartBtn()  {
@@ -119,7 +130,7 @@ function removeStartBtn()  {
 function runQuiz()  {
     removeStartBtn();
     countDown();
-    firstQuestion();
+    loadQuestion();
 }
 answersEl.addEventListener("click", answerBtnHandler);
 startQuizBtn.addEventListener("click", runQuiz);
