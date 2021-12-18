b004cb808d36a2cea9451b79daa61e6c7b770677
Nate Boucher <natejboucher@gmail.com>
2021-12-18 00:38:39 -0500

    added first question function and event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var timerEl = document.getElementById('countdown');
 var questionEl = document.getElementById('questions');
-var answersEl = document.getElementById('quiz-area');
+var answersEl = document.getElementById('answers');
 var startQuizBtn = document.getElementById('start-button');
+var answerAlert = document.getElementById('response-text');
 
 // question objects
 var questionOne = {
     question: 'What does the term HTML stand for?',
     answerOne: 'Hyper Text Pre-processor.',
-    answerTwo: 'Hyper Text Markup Language.',
+    correctAnswer: 'Hyper Text Markup Language.',
     answerThree: 'Hyper Text Multiverse.',
     answerFour: 'Hyper Text Multiple Language.'
 };
@@ -16,13 +17,13 @@ var questionTwo = {
     answerOne: 'Var.',
     answerTwo: 'Object.',
     answerThree: 'Array.',
-    answerFour: 'Boolean.'
+    correctAnswer: 'Boolean.'
 };
 var questionThree = {
     question: 'What is the method of writing our Javascript code in plain english?',
     answerOne: 'A novel.',
     answerTwo: 'Markup Text.',
-    answerThree: 'Pseudocode.',
+    correctAnswer: 'Pseudocode.',
     answerFour: 'Comments.'
 }
 var questionFour = {
@@ -30,7 +31,7 @@ var questionFour = {
     answerOne: 'appendChild.',
     answerTwo: 'getElementById',
     answerThree: 'querySelector',
-    answerFour: 'Media Queries'
+    correctAnswer: 'Media Queries'
 }
 var questionFive = {
     question: 'Which of the following is NOT a Javascript eventListener property?',
@@ -38,6 +39,15 @@ var questionFive = {
     answerTwo: 'mouseover',
     answerThree: 'mouseout',
     answerFour: ':hover'
+}
+
+//answer button handler
+function answerBtnHandler(event)    {
+    if (event.target.matches(".correct-btn")) {
+        answerAlert.textContent = "Correct!"
+    } else  {
+        answerAlert.textContent = "Wrong!";
+    }
 }
 
 // function to control countdown
@@ -58,6 +68,42 @@ function countDown()  {
 }
 
 
+//function to reduce timer and display wrong answer
+
+//function for first question
+function firstQuestion()    {
+    //display question
+    questionEl.textContent = questionOne.question;
+
+    //generate answer buttons
+    var answerOneEl = document.createElement("button");
+    answerOneEl.textContent = questionOne.answerOne;
+    answerOneEl.className = "btn";
+    answersEl.appendChild(answerOneEl);
+
+    var correctAnswer = document.createElement("button");
+    correctAnswer.textContent = questionOne.correctAnswer;
+    correctAnswer.className = "btn correct-btn";
+    answersEl.appendChild(correctAnswer);
+
+    var answerThreeEl = document.createElement("button");
+    answerThreeEl.textContent = questionOne.answerThree;
+    answerThreeEl.className = "btn";
+    answersEl.appendChild(answerThreeEl);
+
+    var answerFourEl = document.createElement("button");
+    answerFourEl.textContent = questionOne.answerFour;
+    answerFourEl.className = "btn";
+    answersEl.appendChild(answerFourEl);
+    //function for selecting answer
+}
+//function for second question
+
+//function for third question
+
+//function for fourth question
+
+//function for fifth question
 
 //function to remove "start quiz" button
 function removeStartBtn()  {
@@ -65,9 +111,11 @@ function removeStartBtn()  {
     taskSelected.remove();
 }
 
+// quiz function
 function runQuiz()  {
     removeStartBtn();
     countDown();
+    firstQuestion();
 }
-
+answersEl.addEventListener("click", answerBtnHandler);
 startQuizBtn.addEventListener("click", runQuiz);
