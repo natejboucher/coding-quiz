0eba91ba653d9f3732aa034f1961481b2a8cf788
Nate Boucher <natejboucher@gmail.com>
2021-12-21 23:11:41 -0500

    updated some styling, added to readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# coding-quiz
-## Timed web development quiz.
+# Coding Quiz
+## About:
+### Timed quiz powered by Javascript that can save your high score and display it on the "High Scores" page.
+#
+## Screenshot:
+![Screenshot of deployed application](./assets/images/screenshot.png)
+#
+## Link to deployed application: 
+
--- assets/js/scorescript.js
+++ assets/js/scorescript.js
@@ -1,25 +1,14 @@
 var scoreParentEl = document.querySelector("#high-scores");
 
-//pull player data from local storage
+//pull player data from local storage and display on page.
 function createScoreEl()    {
-//     var playerData = localStorage.getItem("highScores");
-
-//     playerData = JSON.parse(playerData);
-//     console.log(playerData);
     var playerData = JSON.parse(localStorage.getItem("highScores")) || [];
+    console.log(playerData);
     for (var i = 0; i < playerData.length; i++)  {
 
         var scoreEl = document.createElement("li");
-        scoreEl.textContent = playerData.initials + playerData.score;
+        scoreEl.textContent = playerData[i].initials + " : " + playerData[i].score;
         scoreParentEl.appendChild(scoreEl);
     }
 }
-
-//create score list elements
-// function scoreListEl(playerData)    {
-//     var scoreEl = document.createElement("li");
-//     scoreEl.textContent = playerData.initials + playerData.score;
-//     scoreParentEl.appendChild(scoreEl);
-// }
-
 createScoreEl();
